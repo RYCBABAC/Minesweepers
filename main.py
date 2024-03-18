d1f33ae0ff11c81dee2a3e2533ebbeca256c8f1b
RYCBABAC <itay.chairman@gmail.com>
2024-03-18 19:49:22 +0200

    POC- pygame screen refactor

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame import Surface
 
 bg_color = (192, 192, 192)
 grid_color = (128, 128, 128)
@@ -10,19 +11,31 @@ horizontal_border = (16, 16)
 vertical_border = (100, 16)
 
 
-def main():
-    pygame.init()
-
-    display_size = (grid_size[0] * game_size[0] + horizontal_border[0] + horizontal_border[1],
-                    grid_size[1] * game_size[1] + vertical_border[0] + vertical_border[1])
-    game_display = pygame.display.set_mode(display_size)
-    game_display.fill(bg_color)
-    pygame.display.update()
+def game_loop():
     done = False
     while not done:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
+
+
+def create_display() -> Surface:
+    pygame.init()
+
+    display_size = (grid_size[0] * game_size[0] + horizontal_border[0] + horizontal_border[1],
+                    grid_size[1] * game_size[1] + vertical_border[0] + vertical_border[1])
+    return pygame.display.set_mode(display_size)
+
+
+def update_display_color(game_display: Surface):
+    game_display.fill(bg_color)
+    pygame.display.update()
+
+
+def main():
+    game_display = create_display()
+    update_display_color(game_display)
+    game_loop()
 
 
 if __name__ == "__main__":
