31658f65574a21a9e44934fde7fb6794657426c3
RYCBABAC <itay.chairman@gmail.com>
2024-03-18 20:31:23 +0200

    Dataclasses for cell and cell value

---
+++ cell.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+
+from pygame.rect import RectType
+
+from cell_value import CellValue
+
+
+@dataclass
+class Cell:
+    index: int
+    value: CellValue
+    is_revealed: bool
+    is_flagged: bool
+    rect: RectType
